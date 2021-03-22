5fec81ebc493872a71b18fa41c17f5eb424261a8
Danni Wilton <danniwilton@gmail.com>
2021-03-22 23:36:57 +0800

    update, java still broken

--- script.js
+++ script.js
@@ -6,32 +6,73 @@
 //* confirm Window is generated Symbols to be used Yes or No;
 //* from data given, random passwork is generated;
 //* password shown in password box
+const characterAmountRange = document.getElementById
+('characterAmountRange')
+const characterAmountNumber = document.getElementById
+('characterAmountNumber')
+const includeUppercaseElement = document.getElementById
+('includeUppercase')
+const includeLowercaseElement = document.getElementById
+('includeLowercase')
+const includeNumbersElement = document.getElementById
+('includeNumbers')
+const includeSymbolsElement = document.getElementById
+('includeSymbols')
+const form = document.getElementById ('passwordGeneratorForm')
+
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)
+).concat(
+    arrayFromLowToHigh(91, 96)
+).concat(
+    arrayFromLowToHigh(123, 126)
+)
 
 var submitButtonElement = document.querySelector('#generatepassword');
 var submissionResponseEl = document.querySelector('#password');
 
 // ask for length
-var length = prompt("How many characters long will your password be? Enter a number between 8-126");
-
-//ask if Lowercase will be used
+const aNumber = Number(window.prompt("Type a number between 8-126 for your password length", ""));
 var charType = prompt("Will Lowercase be used? Press Ok for yes, cancel for no.");
 var charType = prompt("Will Uppercase be used? Press Ok for yes, cancel for no.");
 var charType = prompt("Will Numbers be used? Press Ok for yes, cancel for no.");
 var charType = prompt("Will Symbols be used? Press Ok for yes, cancel for no.");
-function functionGeneratePassword(clickEventObject) {
-    clickEventObject.preventDefault();
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const characterAmountNumber = characterAmountNumber.value
+    const includeUppercase = includeUppercaseElement.checked
+    const includeLowercase = includeLowercaseElement.checked
+    const includeNumbers = includeNumbersElement.checked
+    const includeSymbols = includeSymbolsElement.checked
+    const password = generatePassword(characterAmountNumber, includeUppercase, includeLowercase, includeNumbers, includeSymbols)
+})
+
+function generatePassword(characterAmount, includeSymbolsElement, includeLowercase, includeNumbers, includeSymbols) {
+    console.log(LOWERCASE_CHAR_CODES)
 }
+
+function arrayFromLowToHigh(low, high) {
+    const array = []
+    for (let i = low; i <= high; i++) {
+        array.push(i)
+    }
+    return array
+}
+
 // generate password
 function generatePassword() {
     //evaluate character type
     var charSet = "";
-    if( charType.toLowerCase === "Lowercase") {
+    if( charType.includeLowercase === "Lowercase") {
         charSet = "abcdefghijklmnopqrstuvwxyz";
-    } else if( charType.toLowerCase === "Uppercase" ) {
+    } else if( charType.includeUppercase === "Uppercase" ) {
         charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    } else if( charType.toLowerCase === "Numbers" ) {
+    } else if( charType.includeNumbers === "Numbers" ) {
         charSet = "0123456789";
-    } else if( charType.toLowerCase === "Symbols") {
+    } else if( charType.includeSymbols === "Symbols") {
         charSet = " !\#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ";
     }
     // return value
@@ -44,4 +85,3 @@ function generatePassword() {
     console.log(retVal)
     return retVal;
     }
-    alert(generatePassword());
